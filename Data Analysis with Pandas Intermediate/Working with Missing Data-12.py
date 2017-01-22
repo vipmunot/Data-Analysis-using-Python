e2e191aeff557ca06b7d331be379fe7f3bf53885
Vipul Munot <vipmunot@gmail.com>
2017-01-22 13:33:56 -0500

    updating SQL and Probability created

--- Data Analysis with Pandas Intermediate/Working with Missing Data-12.py
+++ Data Analysis with Pandas Intermediate/Working with Missing Data-12.py
@@ -1,34 +1,37 @@
 ## 1. Introduction ##
 
 import pandas as pd
-titanic_survival = pd.read_csv('titanic_survival.csv')
+titanic_survival = pd.read_csv("titanic_survival.csv")
 
 ## 2. Finding the Missing Data ##
 
 age = titanic_survival["age"]
 print(age.loc[10:20])
-age_null_true = age[pd.isnull(age)]
+age_is_null = pd.isnull(age)
+age_null_true = age[age_is_null]
 age_null_count = len(age_null_true)
 print(age_null_count)
 
 ## 3. Whats the big deal with missing data? ##
 
 age_is_null = pd.isnull(titanic_survival["age"])
-age = titanic_survival["age"]
-correct_mean_age = sum(age[~age_is_null])/len(age[~age_is_null])
+good_ages = titanic_survival["age"][age_is_null == False]
+correct_mean_age = sum(good_ages) / len(good_ages)
 
 ## 4. Easier Ways to Do Math ##
 
 correct_mean_age = titanic_survival["age"].mean()
-correct_mean_fare = titanic_survival['fare'].mean()
+correct_mean_fare = titanic_survival["fare"].mean()
 
 ## 5. Calculating Summary Statistics ##
 
 passenger_classes = [1, 2, 3]
 fares_by_class = {}
-for item in passenger_classes:
-    subset = titanic_survival[titanic_survival['pclass']== item]
-    fares_by_class[item] = subset['fare'].mean()
+for this_class in passenger_classes:
+    pclass_rows = titanic_survival[titanic_survival["pclass"] == this_class]
+    pclass_fares = pclass_rows["fare"]
+    fare_for_class = pclass_fares.mean()
+    fares_by_class[this_class] = fare_for_class
 
 ## 6. Making Pivot Tables ##
 
@@ -39,22 +42,22 @@ print(passenger_age)
 ## 7. More Complex Pivot Tables ##
 
 import numpy as np
-port_stats = titanic_survival.pivot_table(index="embarked",values=["survived","fare"],aggfunc=np.sum)
+port_stats = titanic_survival.pivot_table(index="embarked", values=["fare","survived"], aggfunc=np.sum)
 print(port_stats)
 
 ## 8. Drop Missing Values ##
 
 drop_na_rows = titanic_survival.dropna(axis=0)
 drop_na_columns = titanic_survival.dropna(axis=1)
-new_titanic_survival = titanic_survival.dropna(subset = ['sex','age'],axis = 0)
+new_titanic_survival = titanic_survival.dropna(axis=0,subset=["age", "sex"])
 
 ## 9. Using iloc to Access Rows by Position ##
 
 # We have already sorted new_titanic_survival by age
 first_five_rows = new_titanic_survival.iloc[0:5]
-first_ten_rows= new_titanic_survival.iloc[0:10]
-row_position_fifth = new_titanic_survival.iloc[4]
+first_ten_rows = new_titanic_survival.iloc[0:10]
 row_index_25 = new_titanic_survival.loc[25]
+row_position_fifth = new_titanic_survival.iloc[4]
 
 ## 10. Using Column Indexes ##
 
@@ -62,14 +65,15 @@ first_row_first_column = new_titanic_survival.iloc[0,0]
 all_rows_first_three_columns = new_titanic_survival.iloc[:,0:3]
 row__index_83_age = new_titanic_survival.loc[83,"age"]
 row_index_1000_pclass = new_titanic_survival.loc[766,"pclass"]
-row_index_1100_age = new_titanic_survival.loc[1100,'age']
-row_index_25_survived = new_titanic_survival.loc[25,'survived']
-five_rows_three_cols = new_titanic_survival.iloc[:5,:3]
+row_index_1100_age = new_titanic_survival.loc[1100, "age"]
+row_index_25_survived = new_titanic_survival.loc[25, "survived"]
+five_rows_three_cols = new_titanic_survival.iloc[0:5,0:3]
 
 ## 11. Reindexing Rows ##
 
-titanic_reindexed = new_titanic_survival.reset_index(drop = True)
-print(titanic_reindexed.iloc[:5,:3])
+
+titanic_reindexed = new_titanic_survival.reset_index(drop=True)
+print(titanic_reindexed.iloc[0:5,0:3])
 
 ## 12. Apply Functions Over a DataFrame ##
 
@@ -78,22 +82,36 @@ def hundredth_row(column):
     return hundredth_item
 
 hundredth_row = titanic_survival.apply(hundredth_row)
-def null_elements(column):
-    return(len(column[pd.isnull(column)]))
-column_null_count = titanic_survival.apply(null_elements)
+def not_null_count(column):
+    column_null = pd.isnull(column)
+    null = column[column_null]
+    return len(null)
+
+column_null_count = titanic_survival.apply(not_null_count)
 
 ## 13. Applying a Function to a Row ##
 
 def is_minor(row):
     if row["age"] < 18:
-        return "minor"
-    elif row['age'] >=18:
-        return "adult"
+        return True
     else:
-        return "unknown"
+        return False
 
-age_labels = titanic_survival.apply(is_minor, axis=1)
+minors = titanic_survival.apply(is_minor, axis=1)
+import pandas as pd
+
+def generate_age_label(row):
+    age = row["age"]
+    if pd.isnull(age):
+        return "unknown"
+    elif age < 18:
+        return "minor"
+    else:
+        return "adult"
+
+age_labels = titanic_survival.apply(generate_age_label, axis=1)
 
 ## 14. Calculating Survival Percentage by Age Group ##
 
-age_group_survival = titanic_survival.pivot_table(index="age_labels",values='survived')
+
+age_group_survival = titanic_survival.pivot_table(index="age_labels", values="survived")
--- Python Programming Beginner/Introduction to Functions-4.py
+++ Python Programming Beginner/Introduction to Functions-4.py
@@ -1,13 +1,15 @@
 ## 1. Overview ##
 
-vocabulary = open('dictionary.txt').read().split('\n')
+
+f = open("dictionary.txt", "r")
+vocabulary = f.read()
 print(vocabulary)
 
 ## 2. Tokenizing the Vocabulary ##
 
 vocabulary = open("dictionary.txt", "r").read()
-tokenized_vocabulary = vocabulary.split(' ')
-print(tokenized_vocabulary[:5])
+tokenized_vocabulary = vocabulary.split(" ")
+print(tokenized_vocabulary[0:5])
 
 ## 3. Replacing Special Characters ##
 
@@ -17,9 +19,10 @@ story_string = f.read()
 print(story_string)
 story_string = story_string.replace(".","")
 story_string = story_string.replace(",","")
-story_string = story_string.replace("'","")
-story_string = story_string.replace(";","")
-story_string = story_string.replace("\n","")
+story_string = story_string.replace("'", "")
+story_string = story_string.replace(";", "")
+story_string = story_string.replace("\n", "")
+print(story_string)
 
 ## 5. Practice: Creating a Function that Cleans Text ##
 
@@ -28,16 +31,28 @@ story_string = f.read()
 
 def clean_text(text_string):
     cleaned_string = text_string.replace(".","")
-    cleaned_string = cleaned_string.replace(",","")
-    cleaned_string = cleaned_string.replace(";","")
-    cleaned_string = cleaned_string.replace("'","")
-    cleaned_string = cleaned_string.replace("\n","")
-    return(cleaned_string)
 
+cleaned_story = clean_text(story_string)
+# Solution code.
+def clean_text(text_string):
+    cleaned_string = text_string.replace(".","")
+    cleaned_string = cleaned_string.replace(",","")
+    cleaned_string = cleaned_string.replace("'", "")
+    cleaned_string = cleaned_string.replace(";", "")
+    cleaned_string = cleaned_string.replace("\n", "")
+    return(cleaned_string)
 cleaned_story = clean_text(story_string)
 
 ## 6. Changing Word Case ##
 
+def clean_text(text_string):
+    cleaned_string = text_string.replace(",","")
+    cleaned_string = cleaned_string.replace(".","")
+    cleaned_string = cleaned_string.replace("'", "")
+    cleaned_string = cleaned_string.replace(";", "")
+    cleaned_string = cleaned_string.replace("\n", "")
+    return(cleaned_string)
+cleaned_story = clean_text(story_string)
 def clean_text(text_string):
     cleaned_string = text_string.replace(",","")
     cleaned_string = cleaned_string.replace(".","")
@@ -55,14 +70,24 @@ story_string = f.read()
 clean_chars = [",", ".", "'", ";", "\n"]
 
 # Previous code for clean_text().
-def clean_text(text_string,special_characters):
-    cleaned_string = text_string.replace(special_characters[0],"")
-    for item in special_characters:
-        cleaned_string = cleaned_string.replace(item,"")
+def clean_text(text_string):
+    cleaned_string = text_string.replace(",","")
+    cleaned_string = cleaned_string.replace(".","")
+    cleaned_string = cleaned_string.replace("'", "")
+    cleaned_string = cleaned_string.replace(";", "")
+    cleaned_string = cleaned_string.replace("\n", "")
     cleaned_string = cleaned_string.lower()
     return(cleaned_string)
 
-cleaned_story = clean_text(story_string,clean_chars)
+cleaned_story = ""
+def clean_text(text_string, special_characters):
+    cleaned_string = text_string
+    for string in special_characters:
+        cleaned_string = cleaned_string.replace(string, "")
+    cleaned_string = cleaned_string.lower()
+    return(cleaned_string)
+
+cleaned_story = clean_text(story_string, clean_chars)
 print(cleaned_story)
 
 ## 8. Tokenizing the Story ##
@@ -73,14 +98,16 @@ def clean_text(text_string, special_characters):
         cleaned_string = cleaned_string.replace(string, "")
     cleaned_string = cleaned_string.lower()
     return(cleaned_string)
-def tokenize(text_string,special_characters):
-    cleaned_story = clean_text(story_string, clean_chars)
+
+clean_chars = [",", ".", "'", ";", "\n"]
+cleaned_story = clean_text(story_string, clean_chars)
+def tokenize(text_string, special_characters):
+    cleaned_story = clean_text(text_string, special_characters)
     story_tokens = cleaned_story.split(" ")
     return(story_tokens)
-    
-clean_chars = [",", ".", "'", ";", "\n"]
+
 tokenized_story = tokenize(story_string, clean_chars)
-print(tokenized_story[:10])
+print(tokenized_story[0:10])
 
 ## 9. Finding Misspelled Words ##
 
@@ -100,7 +127,7 @@ misspelled_words = []
 clean_chars = [",", ".", "'", ";", "\n"]
 tokenized_story = tokenize(story_string, clean_chars)
 tokenized_vocabulary = tokenize(vocabulary, clean_chars)
-for item in tokenized_story:
-    if item not in tokenized_vocabulary:
-        misspelled_words.append(item)
-print(misspelled_words)        
+for ts in tokenized_story:
+    if ts not in tokenized_vocabulary:
+        misspelled_words.append(ts)
+print(misspelled_words)
