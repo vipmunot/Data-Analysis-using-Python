526c93fe4ba6e53688a8da04ca1f66e057591230
Vipul Munot <vipmunot@gmail.com>
2017-01-25 12:45:57 -0500

    creating machine learning folder

---
+++ Machine learning Beginner/Challenge_ Machine Learning Basics-205.py
@@ -0,0 +1,36 @@
+## 2. Data cleaning ##
+
+import pandas as pd
+columns = ["mpg", "cylinders", "displacement", "horsepower", "weight", "acceleration", "model year", "origin", "car name"]
+cars = pd.read_table("auto-mpg.data", delim_whitespace=True, names=columns)
+filtered_cars = cars[cars['horsepower']!='?']
+filtered_cars['horsepower'] = filtered_cars['horsepower'].astype('float')
+
+## 3. Data Exploration ##
+
+import matplotlib.pyplot as plt
+filtered_cars.plot('horsepower', 'mpg', kind='scatter', c='red')
+filtered_cars.plot('weight', 'mpg', kind='scatter', c='blue')
+plt.show()
+
+## 4. Fitting a model ##
+
+import sklearn
+from sklearn.linear_model import LinearRegression
+lr = LinearRegression()
+lr.fit(filtered_cars[['horsepower']],filtered_cars['mpg'])
+predictions = lr.predict(filtered_cars[['horsepower']])
+print(predictions[:5])
+print(filtered_cars['mpg'].head())
+
+## 5. Plotting the predictions ##
+
+plt.scatter(filtered_cars[['horsepower']],predictions,c = 'b')
+plt.scatter(filtered_cars[['horsepower']],filtered_cars['mpg'],c = 'r')
+plt.show()
+
+## 6. Error metrics ##
+
+from sklearn.metrics import mean_squared_error
+mse = mean_squared_error(filtered_cars['mpg'],predictions)
+rmse = mse ** .5
